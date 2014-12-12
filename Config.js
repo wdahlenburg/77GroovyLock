1c918e14ca4d1d3c01b59b3c46093f4ce7a22a28
wdahlenburg <wddahlenburg@gmail.com>
2014-12-12 17:15:47 -0500

    Updated UI to fit i6 devices.

---
+++ Config.js
@@ -0,0 +1,25 @@
+
+
+var iconSet = "HTC";  //choose "HTC" or "77".
+var locale = "XXXXXX"; //Yahoo Location code.
+var Clock = "12h"; // 12h or 24h.
+
+var lang = "en"; /*
+en= English
+it= Italian
+fr= French
+de= German
+sp= Spanish
+da= Danish
+tr= Turkish */
+
+var tempUnit = "f"; //f=Fahrenheit c=Celsius.
+
+var gps = "false"; 
+var UseCityGPS = "false";
+var UseNeighborhood = "false";
+var updateInterval = "20";
+
+
+
+
